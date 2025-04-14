7117e7b661983c3a1a15f304fc8cd6fcb6c6f2bf
varspijoyan <varsikpijoyan2003@gmail.com>
2025-04-14 21:31:11 +0400

    Format file

--- homeworks/homework_1/script.ts
+++ homeworks/homework_1/script.ts
@@ -50,45 +50,50 @@ type PhoneType = {
   feature: PhoneFeaturesType;
 };
 
-const iphone16Features:PhoneFeaturesType = {
-    launch: {
-        announceDate: {
-            year: 2024,
-            date: 'September 9'
-        },
-        status: {
-            type: 'Available',
-            release: 'September 20'
-        }
+const iphone16Features: PhoneFeaturesType = {
+  launch: {
+    announceDate: {
+      year: 2024,
+      date: "September 9",
     },
-    body: {
-        weight: 170,
-        builds: ['Glass front', 'glass back', 'aluminum frame']
+    status: {
+      type: "Available",
+      release: "September 20",
     },
-    display: {
-        types: ['Super Retina XDR OLED', 'HDR10, Dolby Vision', '1000 nits (typ)', '2000 nits (HBM)'],
-        size: 6.1,
-        protection: 'Ceramic Shield glass (2024 gen)'
-    },
-    platform: {
-        os: 'iOS 18',
-        chipset: 'Apple A18 (3 nm)',
-        cpu: 'Hexa-core',
-        gpu: 'Apple GPU'
-    },
-    memory: {
-        hasCardSlot: false,
-        internals: ['128GB 8GB RAM', '256GB 8GB RAM', '512GB 8GB RAM']
-    }
+  },
+  body: {
+    weight: 170,
+    builds: ["Glass front", "glass back", "aluminum frame"],
+  },
+  display: {
+    types: [
+      "Super Retina XDR OLED",
+      "HDR10, Dolby Vision",
+      "1000 nits (typ)",
+      "2000 nits (HBM)",
+    ],
+    size: 6.1,
+    protection: "Ceramic Shield glass (2024 gen)",
+  },
+  platform: {
+    os: "iOS 18",
+    chipset: "Apple A18 (3 nm)",
+    cpu: "Hexa-core",
+    gpu: "Apple GPU",
+  },
+  memory: {
+    hasCardSlot: false,
+    internals: ["128GB 8GB RAM", "256GB 8GB RAM", "512GB 8GB RAM"],
+  },
 };
 
 const iphone16: PhoneType = {
-    model: 'iPhone 16',
-    colors: ['Black', 'White', 'Pink', 'Teal', 'Ultramarine'],
-    price: 569.81,
-    feature: iphone16Features
+  model: "iPhone 16",
+  colors: ["Black", "White", "Pink", "Teal", "Ultramarine"],
+  price: 569.81,
+  feature: iphone16Features,
 };
 
 console.log(iphone16Features);
-console.log('-----------------------------------------------');
+console.log("-----------------------------------------------");
 console.log(iphone16);
