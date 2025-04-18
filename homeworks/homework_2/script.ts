af6cea235110d793606fa9ace42ee342294dfa04
varspijoyan <varsikpijoyan2003@gmail.com>
2025-04-18 20:50:52 +0400

    Remove comments

--- homeworks/homework_2/script.ts
+++ homeworks/homework_2/script.ts
@@ -64,74 +64,20 @@ class Phone implements IPhone<string, PhoneColor, number, FeatureRecord> {
   model: string;
   color: PhoneColor;
   price: number;
-  // launch: ILaunch<string, number, PhoneStatus>;
-  // body: IBody<number, string[]>;
-  // display: IDisplay<string[], number, string>;
-  // platform: IPlatform<string>;
-  // memory: IMemory<string[], boolean>;
   feature: FeatureRecord;
 
   constructor(
     model: string,
     color: PhoneColor,
     price: number,
-    // launch: ILaunch<string, number, PhoneStatus>,
-    // body: IBody<number, string[]>,
-    // display: IDisplay<string[], number, string>,
-    // platform: IPlatform<string>,
-    // memory: IMemory<string[], boolean>
     feature: FeatureRecord
   ) {
     this.model = model;
     this.color = color;
     this.price = price;
-    // this.launch = launch;
-    // this.body = body;
-    // this.display = display;
-    // this.platform = platform;
-    // this.memory = memory;
     this.feature = feature;
   }
 }
-
-// const _iphone16 = new Phone(
-//   "iphone 16",
-//   "black",
-//   234,
-//   {
-//     announceDate: {
-//       year: 2024,
-//       date: "September 19"
-//     },
-//     status: {
-//       type: "Available",
-//       release: "September 20"
-//     },
-//   },
-//   { weight: 170,
-//     builds: ["Glass front", "glass back", "aluminum frame"]
-//   },
-//   {
-//     types: [
-//       "Super Retina XDR OLED",
-//       "HDR10, Dolby Vision",
-//       "1000 nits (typ)",
-//       "2000 nits (HBM)",
-//     ],
-//     size: 6.1,
-//     protection: "Ceramic Shield glass (2024 gen)"
-//   },
-//   {
-//     os: "iOS 18",
-//     chipset: "Apple A18 (3 nm)",
-//     cpu: "Hexa-core",
-//     gpu: "Apple GPU",
-//   },
-//   {
-//     hasCardSlot: false,
-//     internals: ["128GB 8GB RAM", "256GB 8GB RAM", "512GB 8GB RAM"],
-//   }
-// );
 
 const _iphone16 = new Phone("iPhone 16", "grey", 234, {
   launch: {
